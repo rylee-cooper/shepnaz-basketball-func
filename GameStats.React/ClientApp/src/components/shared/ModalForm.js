fcc52b1b8205ac276173181cd02237e35ebc093c
Rylee Cooper <rylee.shell@yahoo.com>
2021-12-16 15:00:04 -0500

    Updated ModalForm to be functional component

--- GameStats.React/ClientApp/src/components/shared/ModalForm.js
+++ GameStats.React/ClientApp/src/components/shared/ModalForm.js
@@ -1,13 +1,22 @@
-﻿import React, { Component } from 'react';
+﻿import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import Select from 'react-select';
 import { Formik, Field } from 'formik';
 import Modal from 'react-bootstrap/Modal';
 import { inputTypes } from '../../constants';
 
-class ModalForm extends Component {
+const ModalForm = (props) => {
+    const {
+        initialValues,
+        handleFormSubmit,
+        handleCancelClick,
+        addFieldOnSelect,
+        showModal,
+        modalTitle,
+        formFields
+    } = props;
 
-    getCustomStyles = (isValid) => {
+    const getCustomStyles = (isValid) => {
         return {
             control: (styles, state) => ({
                 ...styles,
@@ -19,96 +28,84 @@ class ModalForm extends Component {
         }
     }
 
-    render() {
-        const {
-            initialValues,
-            handleFormSubmit,
-            handleCancelClick,
-            handleSelection,
-            showModal,
-            modalTitle,
-            formFields
-        } = this.props;
-
-        return (
-            <Modal show={showModal}
-                onHide={handleCancelClick}
-                centered
-                backdrop="static"
-                keyboard={false}
-                contentClassName="p-4">
-                <Modal.Header closeButton>
-                    <Modal.Title className="text-dark">{modalTitle}</Modal.Title>
-                </Modal.Header>
-                <Formik
-                    onSubmit={handleFormSubmit}
-                    initialValues={{ ...initialValues }}
-                >
-                    {({
-                        handleSubmit,
-                        handleChange,
-                        values,
-                        touched,
-                        errors,
-                        isSubmitting,
-                    }) => (
-                        <Form onSubmit={handleSubmit}>
-                            <Modal.Body>
-                                <Form.Control.Feedback type="invalid" className="d-block">
-                                    {errors.form}
-                                </Form.Control.Feedback>
-                                {formFields.map(field =>
-                                    !field.isHidden && <Field key={field.name}
-                                        name={field.name}>
-                                        {({ form }) =>
-                                            <Form.Group className="mb-4" key={field.name}>
-                                                {field.inputType === inputTypes.TEXT
-                                                    ? <Form.Control
-                                                        type="text"
+    return (
+        <Modal show={showModal}
+            onHide={handleCancelClick}
+            centered
+            backdrop="static"
+            keyboard={false}
+            contentClassName="p-4">
+            <Modal.Header closeButton>
+                <Modal.Title className="text-dark">{modalTitle}</Modal.Title>
+            </Modal.Header>
+            <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={{ ...initialValues }}
+            >
+                {({
+                    handleSubmit,
+                    handleChange,
+                    values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <Modal.Body>
+                            <Form.Control.Feedback type="invalid" className="d-block">
+                                {errors.form}
+                            </Form.Control.Feedback>
+                            {formFields.map(field =>
+                                !field.isHidden && <Field key={field.name}
+                                    name={field.name}>
+                                    {({ form }) =>
+                                        <Form.Group className="mb-4" key={field.name}>
+                                            {field.inputType === inputTypes.TEXT
+                                                ? <Form.Control
+                                                    type="text"
+                                                    name={field.name}
+                                                    value={values[field.name]}
+                                                    placeholder={field.displayName}
+                                                    onChange={handleChange}
+                                                    isInvalid={touched[field.name] && !!errors[field.name]}
+                                                />
+                                                : field.inputType === inputTypes.SELECT
+                                                    ? <Select
                                                         name={field.name}
-                                                        value={values[field.name]}
+                                                        options={field.options}
                                                         placeholder={field.displayName}
-                                                        onChange={handleChange}
-                                                        isInvalid={touched[field.name] && !!errors[field.name]}
-                                                    />
-                                                    : field.inputType === inputTypes.SELECT
-                                                        ? <Select
-                                                            name={field.name}
-                                                            options={field.options}
-                                                            placeholder={field.displayName}
-                                                            styles={this.getCustomStyles(!errors[field.name])}
-                                                            defaultValue={field.initialValue}
-                                                            closeMenuOnSelect={true}
-                                                            isClearable={true}
-                                                            onChange={(e) => {
-                                                                form.setFieldValue(field.name, e ? e.value : 0, false);
-                                                                if (handleSelection !== undefined) {
-                                                                    handleSelection(e.value, field.name)
-                                                                };
-                                                            }} />
-                                                        : null}
-                                                <Form.Control.Feedback type="invalid" className="d-block">
-                                                    {errors[field.name]}
-                                                </Form.Control.Feedback>
-                                            </Form.Group>
-                                        }
-                                    </Field>
-                                )}
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button variant="secondary" onClick={handleCancelClick}>
-                                    Cancel
-                                </Button>
-                                <Button variant="primary" className="create-btn" type="submit" disabled={isSubmitting}>
-                                    Submit
-                                </Button>
-                            </Modal.Footer>
-                        </Form>
-                    )}
-                </Formik>
-            </Modal>
-        );
-    }
+                                                        styles={getCustomStyles(!errors[field.name])}
+                                                        defaultValue={field.initialValue}
+                                                        closeMenuOnSelect={true}
+                                                        isClearable={true}
+                                                        onChange={(e) => {
+                                                            form.setFieldValue(field.name, e ? e.value : 0, false);
+                                                            if (addFieldOnSelect !== undefined) {
+                                                                addFieldOnSelect(e.value, field.name);
+                                                            };
+                                                        }} />
+                                                    : null}
+                                            <Form.Control.Feedback type="invalid" className="d-block">
+                                                {errors[field.name]}
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    }
+                                </Field>
+                            )}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleCancelClick}>
+                                Cancel
+                            </Button>
+                            <Button variant="primary" className="create-btn" type="submit" disabled={isSubmitting}>
+                                Submit
+                            </Button>
+                        </Modal.Footer>
+                    </Form>
+                )}
+            </Formik>
+        </Modal>
+    );
 }
 
 export default ModalForm;
